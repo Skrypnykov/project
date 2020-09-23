c8633d3d08970e8f51a3f8eea5d972029f908e16
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-09-23 23:18:08 +0300

    update section banner

--- portfolio/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    var docscroll=$(document).scrollTop();
-    if(docscroll>$(window).height()){
-      $('nav').css({'height': $('nav').height(),'width': $('nav').width()}).addClass('fixed');
-    }else{
-      $('nav').removeClass('fixed');
-    }
-});
